47abc3abedf2d6370c70f5e1661044a213572434
Guru <gurunathan2412@gmail.com>
2023-10-17 12:19:42 +0530

    Initial

---
+++ game.py
@@ -0,0 +1,114 @@
+import pygame
+import random
+
+# Constants
+WIDTH, HEIGHT = 600, 600
+GRID_SIZE = 20
+GRID_WIDTH, GRID_HEIGHT = WIDTH // GRID_SIZE, HEIGHT // GRID_SIZE
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+
+# Initialize Pygame
+pygame.init()
+
+# Create the game window
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Snake Game")
+
+# Initialize Snake
+snake = [(5, 5)]
+snake_direction = (1, 0)
+
+# Initialize Food
+food = (random.randint(0, GRID_WIDTH - 1), random.randint(0, GRID_HEIGHT - 1))
+
+# Initialize Score
+score = 0
+
+# Game loop
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP and snake_direction != (0, 1):
+                snake_direction = (0, -1)
+            elif event.key == pygame.K_DOWN and snake_direction != (0, -1):
+                snake_direction = (0, 1)
+            elif event.key == pygame.K_LEFT and snake_direction != (1, 0):
+                snake_direction = (-1, 0)
+            elif event.key == pygame.K_RIGHT and snake_direction != (-1, 0):
+                snake_direction = (1, 0)
+
+    # Move the snake
+    new_head = (snake[0][0] + snake_direction[0], snake[0][1] + snake_direction[1])
+    snake.insert(0, new_head)
+
+    # Check for collisions
+    if snake[0] == food:
+        food = (random.randint(0, GRID_WIDTH - 1), random.randint(0, GRID_HEIGHT - 1))
+        score += 1
+    else:
+        snake.pop()
+
+    if (
+        snake[0][0] < 0
+        or snake[0][0] >= GRID_WIDTH
+        or snake[0][1] < 0
+        or snake[0][1] >= GRID_HEIGHT
+        or snake[0] in snake[1:]
+    ):
+        running = False
+
+    # Clear the screen
+    screen.fill((0, 0, 0))
+
+    # Draw Snake
+    for segment in snake:
+        pygame.draw.rect(
+            screen, GREEN, (segment[0] * GRID_SIZE, segment[1] * GRID_SIZE, GRID_SIZE, GRID_SIZE)
+        )
+
+    # Draw Food
+    pygame.draw.rect(
+        screen, RED, (food[0] * GRID_SIZE, food[1] * GRID_SIZE, GRID_SIZE, GRID_SIZE)
+    )
+
+    # Display Score
+    font = pygame.font.Font(None, 36)
+    score_text = font.render(f"Score: {score}", True, WHITE)
+    screen.blit(score_text, (10, 10))
+
+    # Update the display
+    pygame.display.flip()
+
+    # Cap the frame rate
+    clock.tick(10)  # Adjust the frame rate to control the game speed
+
+# Game over screen
+pygame.quit()
+pygame.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Game Over!")
+
+# Create a font object
+font = pygame.font.Font(None, 36)
+
+# Display "Game Over!" and final score
+game_over_text = font.render("Game Over!", True, WHITE)
+score_text = font.render(f"Final Score: {score}", True, WHITE)
+game_over_rect = game_over_text.get_rect(center=(WIDTH / 2, HEIGHT / 2))
+score_rect = score_text.get_rect(center=(WIDTH / 2, HEIGHT / 2 + 50))
+
+# Show the "Game Over!" message and final score for 2 seconds
+screen.blit(game_over_text, game_over_rect)
+screen.blit(score_text, score_rect)
+pygame.display.flip()
+pygame.time.wait(2000)
+
+# Quit Pygame
+pygame.quit()
